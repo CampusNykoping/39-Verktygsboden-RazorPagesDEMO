a998954c9e378e1ba84fd87bb2e8b80097821055
Claes Engelin <claes.engelin@zocom.se>
2022-04-26 09:51:44 +0200

    Modeller skapade
    User, Admin och Booking klasser

---
+++ Verktygsboden/Models/Booking.cs
@@ -0,0 +1,25 @@
+﻿
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Verktygsboden.Models;
+public class Booking
+{
+    public int Id { get; set; }
+
+    [DisplayName("Noteringar")]
+    public string Notes { get; set; }
+
+    [DisplayName("Från datum")]
+    [DataType(DataType.Date)]
+    public DateTime FromTime { get; set; }
+
+    [DisplayName("Till datum")]
+    public DateTime ToTime { get; set; }
+
+    public virtual Asset? Asset { get; set; }
+    public int AssetId { get; set; }
+    public virtual User? User { get; set; }
+    public int UserId { get; set; }
+
+}
